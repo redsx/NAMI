5acf23accda62249f4b3e6a1c6d59e805b949e79
condy <15979063786@163.com>
2016-12-24 17:14:15 +0800

    build: webpack

---
+++ webpack-prod-config.js
@@ -0,0 +1,50 @@
+const  webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+module.exports = {
+    entry:['babel-polyfill', './app/index.jsx'],
+    output:{
+        path: __dirname + '/server/public/bundle',
+        filename: 'bundle.js',
+    },
+    resolve:{
+        alias:{
+            //...
+        }
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js|\.jsx?$/,
+                exclude: function(input){
+                    if(input.match(/(node_modules|bower_components)/)){
+                        if(input.indexOf("chat-room-plugin")!==-1||input.indexOf("jquery-image-explode")!==-1){
+                           return false;
+                        }
+                        return true;
+                    }
+                    return false;                       
+                },
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015', 'stage-0', 'stage-1'],
+                    plugins: ["transform-decorators-legacy"]
+                }
+            },{
+                test: /\.less$/,
+                loader: "style-loader!css-loader!less-loader?strictMath&noIeCompat"
+            }
+        ]
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        }),
+        //new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('production')
+        })
+  ]
+}
