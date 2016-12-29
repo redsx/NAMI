b8c064f048db876df9b9fb87d3773f532ec14233
condy <15979063786@163.com>
2016-12-29 19:53:56 +0800

    feat: 信息提示

--- app/components/Snackbar.jsx
+++ app/components/Snackbar.jsx
@@ -1,35 +1,30 @@
 import React, {Component} from 'react'
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 import '../less/Snackbar.less'
-class Snackbar extends Component{
-    constructor(props){
-        super(props);
-    }
-    render(){
-        let {snackbars} = this.props;
-        console.log(snackbars.toJS());
-        return (
-            <div className = 'Snackbar-container'>
-            <ReactCSSTransitionGroup
-                component = 'div'
-                transitionName = 'Snackbar'
-                transitionEnterTimeout = {250}
-                transitionLeaveTimeout = {250}
-            >
-            {
-                snackbars.map((item,index)=>{
-                    return (
-                        <div key = {`Snackbar-${index}`}>
-                            <span className = 'Snackbar-content'>
-                                <span> {item}</span>
-                            </span>
-                        </div>
-                    );
-                })
-            }
-            </ReactCSSTransitionGroup>
-            </div>
-        );
-    }
+
+function Snackbar(props){
+    let {snackbars} = props;
+    return (
+        <div className = 'Snackbar-container'>
+        <ReactCSSTransitionGroup
+            component = 'div'
+            transitionName = 'Snackbar'
+            transitionEnterTimeout = {250}
+            transitionLeaveTimeout = {250}
+        >
+        {
+            snackbars.map((item,index)=>{
+                return (
+                    <div key = {`Snackbar-${index}`}>
+                        <span className = 'Snackbar-content'>
+                            <span> {item}</span>
+                        </span>
+                    </div>
+                );
+            })
+        }
+        </ReactCSSTransitionGroup>
+        </div>
+    );
 }
 export default Snackbar;
