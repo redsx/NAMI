350304b9da61cf021c12aee581fc2e16f682ec6a
condy <15979063786@163.com>
2017-01-04 17:05:27 +0800

    feat: recoder audio

---
+++ app/util/recorder.js
@@ -0,0 +1,103 @@
+var recorderWorker = require("worker-loader!./recorderWorker.js");
+
+export class Recorder {
+    config = {
+        bufferLen: 2048,
+        numChannels: 1,
+        mimeType: 'audio/wav'
+    };
+
+    recording = false;
+
+    callbacks = {
+        getBuffer: [],
+        exportWAV: []
+    };
+    constructor(source, cfg) {
+        Object.assign(this.config, cfg);
+        this.context = source.context;
+        this.node = (this.context.createScriptProcessor ||
+        this.context.createJavaScriptNode).call(this.context, this.config.bufferLen, this.config.numChannels, this.config.numChannels);
+
+        this.node.onaudioprocess = (e) => {
+            if (!this.recording) return;
+
+            var buffer = [];
+            for (var channel = 0; channel < this.config.numChannels; channel++) {
+                buffer.push(e.inputBuffer.getChannelData(channel));
+            }
+            this.worker.postMessage({
+                command: 'record',
+                buffer: buffer
+            });
+        };
+
+        source.connect(this.node);
+        this.node.connect(this.context.destination);    //this should not be necessary
+
+        let self = {};
+        this.worker = new recorderWorker();
+
+        this.worker.postMessage({
+            command: 'init',
+            config: {
+                sampleRate: this.context.sampleRate,
+                numChannels: this.config.numChannels
+            }
+        });
+
+        this.worker.onmessage = (e) => {
+            let cb = this.callbacks[e.data.command].pop();
+            if (typeof cb == 'function') {
+                cb(e.data.data);
+            }
+        };
+    }
+
+
+    record() {
+        this.recording = true;
+    }
+
+    stop() {
+        this.recording = false;
+    }
+
+    clear() {
+        this.worker.postMessage({command: 'clear'});
+    }
+
+    getBuffer(cb) {
+        cb = cb || this.config.callback;
+        if (!cb) throw new Error('Callback not set');
+
+        this.callbacks.getBuffer.push(cb);
+
+        this.worker.postMessage({command: 'getBuffer'});
+    }
+
+    exportWAV(cb, mimeType) {
+        mimeType = mimeType || this.config.mimeType;
+        cb = cb || this.config.callback;
+        if (!cb) throw new Error('Callback not set');
+
+        this.callbacks.exportWAV.push(cb);
+
+        this.worker.postMessage({
+            command: 'exportWAV',
+            type: mimeType
+        });
+    }
+
+    forceDownload(blob, filename) {
+        let url = (window.URL || window.webkitURL).createObjectURL(blob);
+        let link = window.document.createElement('a');
+        link.href = url;
+        link.download = filename || 'output.wav';
+        let click = document.createEvent("Event");
+        click.initEvent("click", true, true);
+        link.dispatchEvent(click);
+    }
+}
+
+export default Recorder;
---
+++ app/util/recorderHandle.js
@@ -0,0 +1,77 @@
+import Recorder from './recorder.js'
+
+export default (function (){
+    let audio_context,
+        getUserMedia,
+        recorder,
+        stream,
+        isSupport = true;
+    (function init() {
+        try {
+            window.AudioContext = window.AudioContext || window.webkitAudioContext;
+            // window.URL = window.URL || window.webkitURL;
+            getUserMedia = initUserMediaHandle();
+            audio_context = new AudioContext;
+        } catch (e) {
+            isSupport = false;
+            console.log('No web audio support in this browser!');
+        }
+        
+    })();
+    function initUserMediaHandle(){
+        if(!navigator.mediaDevices) navigator.mediaDevices = {};
+        if (!navigator.mediaDevices.getUserMedia) {
+            navigator.mediaDevices.getUserMedia = function(constraints) {
+                var getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+                isSupport = !!getUserMedia;
+                if (!getUserMedia) return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+                return new Promise(function(resolve, reject) {
+                    getUserMedia.call(navigator, constraints, resolve, reject);
+                });
+            }
+        }
+        return navigator.mediaDevices.getUserMedia;
+    }
+    function startRecording() {
+        recorder && recorder.record();
+    }
+    function stopRecording() {
+        recorder && recorder.stop();
+        recorder.clear();
+    }
+    function startUserMedia() {
+        console.log('getUserMedia: ',getUserMedia);
+        if(isSupport){
+            navigator.mediaDevices.getUserMedia({audio: true})
+            .then((inputStream) => {
+                stream = inputStream;
+                let input = audio_context.createMediaStreamSource(inputStream);
+                input.connect(audio_context.destination);
+                recorder = new Recorder(input);
+                startRecording();
+            })
+            .catch((err)=>{
+                console.log(err);
+            })
+        }
+    }
+    function stopUserMedia(){
+        stopRecording()
+        if(stream){
+            let tracks = stream.getTracks();
+            for(let i=0; i<tracks.length; i++){
+                tracks[i].stop();
+            }
+        }
+    }
+    function createDownloadLink() {
+        return new Promise((resolve,reject) => {
+            recorder && recorder.exportWAV(function(blob) {
+                // var url = URL.createObjectURL(blob);
+                console.log('blob: ',blob);
+                resolve(blob);
+            });
+        })
+    }
+    return {startUserMedia,stopUserMedia,createDownloadLink,isSupport}
+})()
