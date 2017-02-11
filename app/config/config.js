f536bd4763ddaaace85965925708bcf233944a3c
condy <15979063786@163.com>
2017-02-11 11:48:10 +0800

    feat: config

---
+++ app/config/config.js
@@ -0,0 +1,8 @@
+export default {
+    MaxImageSize: 3*1024*1024, 
+    CompressAvatarSize: 100,
+    ImageStyle: 'imageView2/2/w/169/interlace/0/q/50', //七牛图片处理样式
+    LoadingImage: 'https://ooo.0o0.ooo/2017/01/17/587dd9f639872.gif',
+    defaultGroupAvatar: 'http://oj7h98lzb.bkt.clouddn.com/download.svg',
+    ScreenMessageLenght: 15,
+}
---
+++ app/config/socketConfig.js
@@ -0,0 +1,10 @@
+export default {
+    development: {
+        HOST: 'localhost',
+        PORT: '3002'
+    },
+    production: {
+        HOST: 'mdzzapp.com',
+        PORT: '3002'
+    },
+}
