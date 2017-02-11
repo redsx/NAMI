eca3b23c1e5b889fe89d6679575f233c05efde9d
condy <15979063786@163.com>
2017-02-11 11:50:44 +0800

    feat: middlewares

---
+++ app/middlewares/message.js
@@ -0,0 +1,25 @@
+import immutable from 'immutable'
+export default {
+    priToGro: function(message,room,user){
+        if(message.get('owner')) return message;
+        const userId = user.get('_id'),
+              fromId = message.get('from'),
+              roomId = room.get('_id');
+        let owner;
+        if(fromId === roomId){ 
+            owner = immutable.fromJS({
+                _id: roomId,
+                avatar: room.get('avatar'),
+                nickname: room.get('name'),
+            });
+        }
+        if(fromId === userId) {
+            owner = immutable.fromJS({
+                _id: userId,
+                avatar: user.get('avatar'),
+                nickname: user.get('nickname'),
+            });
+        }
+        return message.set('owner',owner);
+    }
+}
---
+++ app/middlewares/schemas.js
@@ -0,0 +1,32 @@
+import { schema, normalize } from 'normalizr'
+
+const history = new schema.Entity('histories',{},{ idAttribute: '_id' });
+const room = new schema.Entity('rooms', {histories: [history]}, { idAttribute: '_id' });
+
+export const roomsSchema = [room]
+
+const user = new schema.Entity('rooms',{},{ 
+    idAttribute: '_id',
+    processStrategy: (entity) => {
+        entity.name = entity.nickname;
+        entity.isPrivate = true;
+        delete entity.nickname; 
+        return entity
+    }
+ });
+const privateM = new schema.Entity('histories',{from: user},{ idAttribute: '_id' });
+
+export const privateSchema = [privateM];
+export const historySchema = [history];
+
+export const getInitPrivateData = (data) => {
+    const normalizeData = normalize(data,privateSchema);
+    console.log(normalizeData);
+    const { entities, result } = normalizeData;
+    let { rooms, histories } = entities;
+    result.forEach((key) => {
+        const from = histories[key].from;
+        rooms[from]['histories'] = rooms[from]['histories'] ? rooms[from]['histories'].concat(key) : [key];
+    })
+    return { rooms, histories };
+}
