35282a40c0a6f1a124eab27f1d8f8541a3e0d8c6
condy <15979063786@163.com>
2017-02-11 11:52:01 +0800

    feat: olugins

---
+++ app/plugins/PureRender.js
@@ -0,0 +1,35 @@
+import Immutable from 'immutable'
+import React, { Component } from 'react'
+
+export const shouldComponentUpdate = function(nextProps = {}, nextState = {}) {
+  nextState = nextState || {};
+  nextProps = nextProps || {};
+  const thisProps = this.props || {}, thisState = this.state || {};
+  if (Object.keys(thisProps).length !== Object.keys(nextProps).length ||
+      Object.keys(thisState).length !== Object.keys(nextState).length) {
+      return true;
+  }
+  for (const key in nextProps) {
+      if (!Immutable.is(thisProps[key], nextProps[key])) {
+        //   console.log(key + ' is not equal');
+          return true;
+      }
+  }
+  for (const key in nextState) {
+      if (!Immutable.is(thisState[key], nextState[key])) {
+          return true;
+      }
+  }
+      return false;
+}
+
+const PureRender = (WrappedComponent) => class extends Component{
+    constructor(props){
+        super(props);
+        this.shouldComponentUpdate = shouldComponentUpdate.bind(this);
+    }
+    render(){
+        return <WrappedComponent {...this.props} />
+    }
+}
+export default PureRender;
