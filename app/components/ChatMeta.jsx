5019abd244efe9e788bb6a24aa18f72ecd9759d1
condy <15979063786@163.com>
2017-02-11 11:37:25 +0800

    feat: ChatMeta

---
+++ app/components/ChatMeta.jsx
@@ -0,0 +1,22 @@
+import React , {Component} from 'react'
+import PureRender from '../plugins/PureRender.js'
+import '../less/ChatMeta.less'
+
+function ChatMeta(props){
+    console.log('chatmeta');
+    let { title, secondary, time, marker, Menu } = props;
+    return (
+        <div className = 'ChatMeta'>
+            <div className = 'ChatMeta-main'>
+                <span className = 'ChatMeta-title'>{title}</span>
+                <span className = {marker?'ChatMeta-marker':'ChatMeta-time'}>{ marker || time }</span>
+            </div>
+            <div className = 'ChatMeta-secondary'>
+                <span className = 'ChatMeta-secondary-text'>{secondary}</span>
+                <span className = 'ChatMeta-menu-btn'>{ Menu }</span>
+            </div>
+        </div>
+    );
+}
+
+export default PureRender(ChatMeta);
