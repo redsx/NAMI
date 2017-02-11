e0dfba343b3001e0205a600e83f557e9fc58078a
condy <15979063786@163.com>
2017-02-11 11:31:27 +0800

    feat: invite

---
+++ app/components/Invite.jsx
@@ -0,0 +1,46 @@
+import React , {Component} from 'react'
+import immutable from 'immutable'
+import language from '../config/language.js'
+import { setRightManager } from '../actions/pageUI.js'
+import Avatar from './Avatar.jsx'
+import Header from './Header.jsx'
+import ContentEditableInput from './ContentEditableInput.jsx'
+import ListItem from './ListItem.jsx'
+import timedeal from '../util/time.js'
+import Loading from './Loading.jsx'
+import DelayedAnimation from './DelayedAnimation.jsx'
+import ProfileSection from './ProfileSection.jsx'
+import ProfileButton from './ProfileButton.jsx'
+import '../less/Invite.less'
+
+function Invite(props){
+    const { isLoading, room, handleCopyLink, handleRefreshLink } = props;
+    return (
+            <div className = 'Profile-container'>
+                <Header 
+                    title = {language.inviteTitle}
+                    leftElement = {<i className = 'icon' onClick = {()=>setRightManager({isShow:true,state: 'profile'})}>&#xe611;</i>}
+                />
+                <div className = 'Profile-body'>
+
+                    <ProfileSection className = 'Invite-Section'>
+                        <Avatar size = {82} src = {room.get('avatar')}/>
+                        <div className = 'Invite-link-container'>
+                            <p>{room.get('name')}</p>
+                            <p className = 'Invite-link'>{room.get('link')}</p>
+                        </div>
+                    </ProfileSection>
+
+                    <div className = 'Profile-transparent'>{language.userProfileTran}</div>
+
+                    <div className = 'UserProfile-margin'>
+                        <ProfileButton text = '使用 nami 发送链接' unicode = '&#xe61b;' color = 'rgb(101, 102, 103)'/>
+                        <ProfileButton text = '复制链接' unicode = '&#xe650;' handleClick = {handleCopyLink} color = 'rgb(101, 102, 103)'/>
+                        <ProfileButton text = '更新链接' unicode = '&#xe751;' handleClick = {handleRefreshLink} color = 'rgb(101, 102, 103)' />
+                    </div>
+                </div>  
+            </div>
+    );
+}
+
+export default Invite;
