ef5b36a0cc3b3824a9066242f8076f32a3221199
condy <15979063786@163.com>
2017-02-11 11:55:34 +0800

    feat: config

---
+++ server/config/cr-config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    ROBOT_NAME: '机器人小z',
+    INIT_ROOM: 'MDZZ',
+    INIT_ROOM_INFO: '有问题欢迎提issue',
+    INIT_ADMIN_EMAIL: 'admin@mdzzapp.com',
+    DEFAULT_AVATAR: 'http://oj7h98lzb.bkt.clouddn.com/user.svg',
+    DAY: 24*60*60*1000,
+    JWT_KEY: 'JWT_KEY',
+    ROBOT_URL: 'http://www.tuling123.com/openapi/api',
+    ROBOT_KEY: '273e3ca4189a3fc03b4a94db8d87796b',
+    ROBOT_AVATAR:'http://oajmk96un.bkt.clouddn.com/robot.jpg',
+    QINIU_ACCESS_KEY: 'WfabXLWGFotFsoyqTz-fL7gmD_wVjtNGa3sTmE3O',
+    QINIU_SECRET_KEY: '8i3OPlKmgURNH3WQQjqbga-tYwN0r92XnnBdx8Ie',
+    QINIU_CNAME: 'http://oj7h98lzb.bkt.clouddn.com',
+    QINIU_BUCKET: 'mdzz',
+    MAX_ROOM: 3,
+    AVATAR_ARR: ['呵呵', '哈哈', '吐舌', '啊', '酷', '怒', '开心', '汗', '泪', '黑线',
+                     '鄙视', '不高兴', '真棒', '钱', '疑问', '阴险', '吐', '咦', '委屈', '花心', 
+                     '呼', '笑眼', '冷', '太开心', '滑稽', '勉强', '狂汗', '乖', '睡觉', '惊哭',
+                     '升起', '惊讶', '喷', '爱心', '心碎', '玫瑰', '礼物', '彩虹', '星星月亮', '太阳']
+}
