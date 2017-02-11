5f0f182c5f430d85d1be65be6ea1926b9987ecea
condy <15979063786@163.com>
2017-02-11 11:52:49 +0800

    feat: store

---
+++ app/store/index.js
@@ -0,0 +1,19 @@
+import {createStore,applyMiddleware,compose} from 'redux'
+import thunk from 'redux-thunk'
+import reducer from '../reducers'
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+let store;
+if(process.env.NODE_ENV === 'development'){
+    store = createStore(reducer, composeWithDevTools(
+        applyMiddleware(thunk)
+    ));
+} else{
+    const finalCreactStore = applyMiddleware(thunk)(createStore);
+    store = finalCreactStore(reducer);
+}
+let unsubscribe = store.subscribe(() =>{
+    // console.log('store监控:', store.getState().toJS())
+    }
+);
+export default store;
