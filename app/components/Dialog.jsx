55ad0777e8f6a4739d2663b74f8ce196eaecba24
condy <15979063786@163.com>
2017-02-11 11:30:30 +0800

    feat: Dialog

---
+++ app/components/Dialog.jsx
@@ -0,0 +1,11 @@
+import React , {Component} from 'react'
+
+function Dialog(props){
+    console.log('props.params.invite');
+    console.log(props.params.link);
+    return (
+        <div></div>
+    );
+}
+
+export default Dialog;
