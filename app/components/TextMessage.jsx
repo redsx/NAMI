b0242869d2638a6b0859ce8d504088c3e6f9b0c9
condy <15979063786@163.com>
2017-02-11 11:32:26 +0800

    feat: TextMessage

---
+++ app/components/TextMessage.jsx
@@ -0,0 +1,23 @@
+import React , {PropTypes, Component} from 'react'
+import PureRender from '../plugins/PureRender.js'
+import MessageBox from './MessageBox.jsx'
+import '../less/SignOwl.less'
+
+function replaceContent(content){
+    let repContent = content || '';
+    const regLink = /^((http|https|ftp):\/\/)?(\w(\:\w)?@)?([0-9a-z_-]+\.)*?([a-z0-9-]+\.[a-z]{2,6}(\.[a-z]{2})?(\:[0-9]{2,6})?)((\/[^?#<>\/\\*":]*)+(\?[^#]*)?(#.*)?)?$/i;
+    // const regImg = /https?:\/\/.+\.(jpg|gif|png|svg|jpeg)/i
+    repContent = repContent.replace(regLink, r => `<a href="${r}" target="_blank" rel="noreferrer">${r}</a>`);
+    return repContent;
+}
+function TextMessage(props){
+    console.log('textMessage');
+    const content = replaceContent(props.content.get('content'));
+    return(
+        <MessageBox {...props}>
+            <span  dangerouslySetInnerHTML={ {__html: content}}></span>
+        </MessageBox>
+    );
+}
+
+export default PureRender(TextMessage);
