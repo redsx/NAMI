08fd25b4342149e0a4ba00610a68e92306f6bee3
condy <15979063786@163.com>
2017-02-11 11:38:51 +0800

    feat: IconAvatar

---
+++ app/components/IconAvatar.jsx
@@ -0,0 +1,25 @@
+import React , {Component} from 'react'
+import PureRender from '../plugins/PureRender.js'
+import uploadHandle from '../util/upload.js'
+import '../less/Avatar.less'
+
+function IconAvatar(props){
+    console.log('avatar');
+    let { size, handleClick, radius, avatarBG, unicode } = props;
+    size = size || 39;
+    const avatarContainerStyle = {
+        width: size + 'px',
+        height: size + 'px',
+        lineHeight: size + 'px',
+        fontSize: size/2 + 'px',
+        background: avatarBG || '#128C7E',
+        borderRadius: radius + 'px' || '50%'
+    }
+    return (
+        <div className = 'IconAvatar' style = {avatarContainerStyle}>
+            <i className = 'icon'>{unicode}</i>
+        </div>
+    );
+}
+
+export default PureRender(IconAvatar);
