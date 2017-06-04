567f233f6724a2e070765ee41e2acd2ccb8f58a2
condy <15979063786@163.com>
2017-06-04 12:23:19 +0800

    fix: 修复文件上传保留缓存文件问题

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -1,15 +1,17 @@
 var router = require('koa-router')()
   , body = require('koa-better-body')
+  , fs = require('fs')
   , uploadFile = require('../util/uploadImage');
 
 router.post('/file',body(),function*(next){
+    const targetPath = './tmp/upload';
     let file = this.request.files[0];
     let type = file.name.split('.');
     let key = file.path.split('/');
     key = key[key.length-1];
     if(type.length > 1) key = key + '.' + type[type.length-1];
-    console.log(key);
     let ret = yield uploadFile(key,file.path);
+    fs.rename(file.path, targetPath, (err)=>console.error(err));
     this.body = ret;
 })
 
